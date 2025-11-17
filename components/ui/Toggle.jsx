18fca4b778161afdb00d03b7810f5256141332c8
LauRRaud <raudsoolaur@gmail.com>
2025-11-17 03:34:43 +0200

    SotsiaalAI

--- components/ui/Toggle.jsx
+++ components/ui/Toggle.jsx
@@ -1,30 +1,30 @@
-\"use client\";
-import EyeIcon from \"@/public/logo/silma.svg\";
+"use client";
+import EyeIcon from "@/public/logo/silma.svg";
 
-export default function Toggle({ id = \"toggle\", checked, onChange, ariaDescribedBy }) {
+export default function Toggle({ id = "toggle", checked, onChange, ariaDescribedBy }) {
   const handleChange = (event) => {
     const next = event.target.checked;
     if (onChange) onChange(next);
   };
 
-  const inputId = id || \"toggle\";
+  const inputId = id || "toggle";
 
   return (
-    <label className=\"switch-button\" htmlFor={inputId}>
-      <div className=\"switch-outer\">
+    <label className="switch-button" htmlFor={inputId}>
+      <div className="switch-outer">
         <input
           id={inputId}
-          type=\"checkbox\"
+          type="checkbox"
           checked={!!checked}
           onChange={handleChange}
           aria-describedby={ariaDescribedBy}
-          className=\"switch-input\"
+          className="switch-input"
         />
-        <div className=\"button\">
-          <span className=\"button-toggle\">
-            <EyeIcon aria-hidden=\"true\" className=\"switch-eye-icon\" />
+        <div className="button">
+          <span className="button-toggle">
+            <EyeIcon aria-hidden="true" className="switch-eye-icon" />
           </span>
-          <span className=\"button-indicator\" />
+          <span className="button-indicator" />
         </div>
       </div>
     </label>
