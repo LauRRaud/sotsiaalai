e0277049c95cbe9bac9047984d5278e33a0fa44c
LauRRaud <raudsoolaur@gmail.com>
2025-11-18 10:59:44 +0200

    SotsiaalAI

--- scripts/print-unused.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const root = path.resolve(__dirname,'..');
-const reportPath = path.join(root,'scripts','unused-css-report.json');
-const r = JSON.parse(fs.readFileSync(reportPath,'utf8'));
-const unused = r.filter(x=>x.count===0).map(x=>x.selector);
-console.log('Unused selectors (count='+unused.length+')');
-for(const u of unused) console.log(u);
