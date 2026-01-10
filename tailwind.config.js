bb0c87f62b346cc92c9d5ab276fdd162d2cd8268
LauRRaud <raudsoolaur@gmail.com>
2026-01-10 12:05:18 +0200

    chore(tailwind): add config and wire tailwind into global styles

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,mdx}",
+    "./components/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
