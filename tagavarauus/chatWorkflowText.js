2b1b7dac0a2e849dfc9c74fbbcaef529c100a9e1
LauRRaud <raudsoolaur@gmail.com>
2026-04-06 16:27:02 +0300

    ...

--- tagavarauus/chatWorkflowText.js
+++
@@ -1,9 +0,0 @@
-import { serverT } from "../i18n/serverMessages.js";
-
-export function helpWorkflowT(locale, key, values, fallback = "") {
-  return serverT(locale, `chat.helpWorkflow.${key}`, values, fallback);
-}
-
-export function helpWorkflowLabel(locale, key, fallback = "") {
-  return helpWorkflowT(locale, `labels.${key}`, undefined, fallback);
-}
